ebe548982a845ba537e87225fc9a8ea2335e146c
Quentin Engles <hollowdoor99@gmail.com>
2017-05-01 19:52:18 -0500

    added tests

---
+++ test/run.js
@@ -0,0 +1,10 @@
+//Notice the call right after the require.
+const rll = require('../')();
+
+rll`Hello ${'who? '}!`.then((result)=>{
+    //You will be prompted with "who? "
+    //result is the interpolated string with all of your input.
+    console.log(result);
+}, (err) => {
+    console.log(err)
+});
